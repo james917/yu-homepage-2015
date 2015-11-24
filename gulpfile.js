39d44c4f79f2ae86774ef1b0b09f4368229c7e49
James Leone <james.leone@yu.edu>
2015-11-24 17:36:36 -0500

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -6,31 +6,63 @@ var gulp = require('gulp'),
     webserver = require('gulp-webserver');
 
 
-
-  gulp.task('less', function(){
-    gulp.src('./bower_components/bootstrap/less/bootstrap.less')
+    gulp.task('less', function(){
+    gulp.src('less/*.less')
       .pipe(less({
         paths: [path.join(__dirname, 'less', 'includes')]
       }))
       .pipe(less())
       .pipe(gulp.dest('public/css'))
-    // console.log('works less');
+
   });
 
+
   // gulp.task('scripts', function(){
-    // console.log('js works');
-    //gulp.src([''])
+  //   gulp.src(['./bower_components/bootstrap/js/transition.js',
+  //         './bower_components/bootstrap/js/alert.js',
+  //         './bower_components/bootstrap/js/button.js',
+  //         './bower_components/bootstrap/js/carousel.js',
+  //         './bower_components/bootstrap/js/collapse.js',
+  //         './bower_components/bootstrap/js/dropdown.js',
+  //         './bower_components/bootstrap/js/modal.js',
+  //         './bower_components/bootstrap/js/tooltip.js',
+  //         './bower_components/bootstrap/js/popover.js',
+  //         './bower_components/bootstrap/js/scrollspy.js',
+  //         './bower_components/bootstrap/js/tab.js',
+  //         './bower_components/bootstrap/js/affix.js'])
   // .pipe(concat('main.js'))
   // .pipe(gulp.dest('public/js'))
   // });
+
+
+
+gulp.task('scripts', function() {
+  gulp.src([
+          './bower_components/jquery/dist/jquery.js'])
+  .pipe(concat('jquery.js'))
+  .pipe(gulp.dest('public/js'))
+
+});
+
+
+// gulp.task('scripts', function() {
+//   gulp.src(['libs/holder.js', 'ie-emulation-modes-warning.js', 'ie10-viewport-bug-workaround.js'])
+//   .pipe(concat('extra.js'))
+//   .pipe(gulp.dest('public/js'))
+
+// });
+
 
  gulp.task('html', function(){
       gulp.src('public/index.html')
   });
 
 gulp.task('watch', function(){
-  gulp.watch('./bower_components/bootstrap/less/*.less', ['less']);
+  gulp.watch('less/*.less', ['less']);
   // gulp.watch('builds/app/js/**/*.js', ['scripts']);
+  // gulp.watch('./bower_components/bootstrap/js/*.js', ['scripts']);
+  gulp.watch('./bower_components/jquery/dist/*.js', ['scripts']);
+  // gulp.watch('libs/*.js', ['scripts']);
   gulp.watch(['public/index.html','public/index.html'], ['html']);
 });
 
@@ -44,4 +76,4 @@ gulp.task('watch', function(){
   });
 
 
-  gulp.task('default', ['watch', 'less', 'html', 'webserver' ]);
+  gulp.task('default', ['watch', 'less', 'scripts', 'html', 'webserver' ]);
